b7bec1f750f8c54ecdc9a40d1a1257052b927e4b
Anderson Douglas <anderson.devsan@gmail.com>
2020-08-15 17:53:44 -0300

    creat movies and Rows

--- README.md
+++ README.md
@@ -4,12 +4,8 @@
 - Get TMDB API key :heavy_check_mark:
 - Create a react app :heavy_check_mark:
 - Setup firebase hosting :heavy_check_mark:
-- Get all Movies:point_left:
-- Build the Rows
+- Get all Movies :point_left:
+- Build the Rows :point_left:
 - Build the Banner
 - Build Nav bar
 - Add trailer popups
-
-
-<!-- https://api.themoviedb.org/3/movie/550?api_key=0e1af2366f7ce761d185564b6d36d63f -->
-<!-- https://netflix-clone-8e7a6.web.app/ -->
--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import request from './requests';
 
 function App() {
   return (
     <div className="App">
        <h1>Contruindo o front-end do clone da Netdlix</h1>
+       <Row title="Netflix Original" fetchUrl ={request.fecthNetflixOriginals} />
+       <Row title="Trending Now" fetchUrl ={request.fetchTrending} />
+
+
     </div>
   );
 }
 
 export default App;
+ 
